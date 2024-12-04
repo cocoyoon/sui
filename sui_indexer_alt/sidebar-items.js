ea72b01957f034128f9cdf66254a4f8bbe7f3533
cocoyoon <cocoyoon@users.noreply.github.com>
2024-12-04 13:24:27 +0000

    deploy: 5c8f373868d371ea467ebc1de7888109cf9e8797

---
+++ sui_indexer_alt/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["start_indexer"],"mod":["args","bootstrap","config","db","handlers","ingestion","metrics","models","pipeline","schema","task"],"struct":["Indexer","IndexerArgs"]};
